7f4cdee3bb6a8c3cf03a81d49d4b6fd14937bc73
amrit raj <amritrajsingh179@gmail.com>
2025-04-28 20:05:54 +0530

    numeric triangle

--- basic.pattern.js
+++ basic.pattern.js
@@ -87,4 +87,19 @@
 // console.log();
 // }
 
+// 1 
+// 2 3
+// 4 5 6
+// 7 8 9 10
  
+// let n=4;
+// let num=1;
+// for(let i=1;i<=n;i++){
+//     for(let j=1;j<=i;j++){
+//         process.stdout.write(num.toString()+" ");
+//         num++;
+//     }
+   
+//     console.log();
+    
+// }
