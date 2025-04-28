dbc8b4a95e4a453690c86982228a3947fdd2ea2b
amrit raj <amritrajsingh179@gmail.com>
2025-04-28 19:27:41 +0530

    diamond

--- basic.pattern.js
+++ basic.pattern.js
@@ -17,13 +17,39 @@
 // ****
 // *****
 
-let nst=1;
-let n=5
-for(let i=0;i<=n;i++){
-    for(let j=1;j<nst;j++){
-        process.stdout.write("*");
-    }
-    nst++;
-    console.log();
+// let nst=1;
+// let n=5
+// for(let i=0;i<=n;i++){
+//     for(let j=1;j<nst;j++){
+//         process.stdout.write("*");
+//     }
+//     nst++;
+//     console.log();
     
-}
+// }
+//   *
+//  ***
+// *****
+//  ***
+//   *
+
+// let n=5;
+// let nsp=n/2;
+// let nst=1;
+
+// for(let i=1;i<=n;i++){
+// for(let j=1;j<=nsp;j++){
+//     process.stdout.write(" ");
+// }
+// for(let k=1;k<=nst;k++){
+//     process.stdout.write("*");
+// }
+// if(i<=n/2){
+//     nst+=2;
+//     nsp--;
+// }else{
+//     nsp++;
+//     nst-=2;
+// }
+// console.log();
+// }
