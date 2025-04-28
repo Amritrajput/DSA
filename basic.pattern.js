a35d35e1a8e2610bd74d0a38f331b72d0d9f5089
amrit raj <amritrajsingh179@gmail.com>
2025-04-28 19:55:35 +0530

    hollow diamond

--- basic.pattern.js
+++ basic.pattern.js
@@ -53,3 +53,38 @@
 // }
 // console.log();
 // }
+
+// **** ****
+// ***   ***
+// **     **
+// *       *
+// **     **
+// ***   ***
+// **** ****
+
+
+// let n=7;
+// let nsp=1;
+// let nst=n/2+1;
+
+// for(let i=1;i<=n;i++){
+// for(let j=1;j<=nst;j++){
+//     process.stdout.write("*");
+// }
+// for(let k=1;k<=nsp;k++){
+//     process.stdout.write(" ");
+// }
+// for(let j=1;j<=nst;j++){
+//     process.stdout.write("*");
+// }
+// if(i<=n/2){
+//     nsp+=2;
+//     nst--;
+// }else{
+//     nst++;
+//     nsp-=2;
+// }
+// console.log();
+// }
+
+ 
