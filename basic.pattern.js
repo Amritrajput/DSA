81086768bb1a297a0fb4a4e39cdb743143b4adfa
amrit raj <amritrajsingh179@gmail.com>
2025-04-28 18:57:18 +0530

    2nd pattern

--- basic.pattern.js
+++ basic.pattern.js
@@ -3,10 +3,27 @@
 // ****
 // ****
 
-let n=4;
-for(let i=0;i<n;i++){
-    for(let j=0;j<n;j++){
-       process.stdout.write("*");
+// let n=4;
+// for(let i=0;i<n;i++){
+//     for(let j=0;j<n;j++){
+//        process.stdout.write("*");
+//     }
+//     console.log();
+// }
+
+// *
+// **
+// ***
+// ****
+// *****
+
+let nst=1;
+let n=5
+for(let i=0;i<=n;i++){
+    for(let j=1;j<nst;j++){
+        process.stdout.write("*");
     }
+    nst++;
     console.log();
+    
 }
