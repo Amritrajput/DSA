838c003bdbf2dd9e5ebde1e931e81ad61ffe7088
amrit raj <amritrajsingh179@gmail.com>
2025-04-30 18:09:35 +0530

    c

--- mathsproblem.js
+++ mathsproblem.js
@@ -1,8 +1,8 @@
-let n = 20;
-let arr=[];
-for(let i=1;i<=n;i++){
-    if(n%i===0){
-        arr.push(i);
-    }
-}
-console.log(arr);
+// let n = 20;
+// let arr=[];
+// for(let i=1;i<=n;i++){
+//     if(n%i===0){
+//         arr.push(i);
+//     }
+// }
+// console.log(arr);
--- recursionbasic.js
+++ recursionbasic.js
@@ -29,3 +29,5 @@
 //     printname()
 // }
 // name()
+
+
