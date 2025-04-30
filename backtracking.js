9ff43f18c8a46d2660382a96d7d675fc5f86bb8d
amrit raj <amritrajsingh179@gmail.com>
2025-04-30 18:57:31 +0530

    reverseorder

--- backtracking.js
+++ backtracking.js
@@ -1,9 +1,20 @@
- function backtrackcount(n){
+//  function backtrackcount(n){
+//     function count(i){
+//         if(i<1) return;
+//         count(i-1);
+//         console.log(i);
+
+//     }
+//     count(n);
+//  }
+//  backtrackcount(5)
+
+
+function backtrackcount(n){
     function count(i){
         if(i<1) return;
-        count(i-1);
         console.log(i);
-
+        count(i-1);
     }
     count(n);
  }
