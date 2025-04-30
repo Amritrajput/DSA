f546e02bba5dfc5ecc9ca2a007b439dbe0e46a97
amrit raj <amritrajsingh179@gmail.com>
2025-04-30 18:07:34 +0530

    rec

--- recursionbasic.js
+++ recursionbasic.js
@@ -1,13 +1,31 @@
 // Q.count till 1 to 3
-function counting(){
-    let count=1;
+
+
+// function counting(){
+//     let count=1;
     
-    function counts(){
-        if(count===4) return;
-        console.log(count);
-        count++;
-        counts();
-    }
-    counts();
-}
-counting()
+//     function counts(){
+//         if(count===4) return;
+//         console.log(count);
+//         count++;
+//         counts();
+//     }
+//     counts();
+// }
+// counting()
+
+
+// print name 5 times
+
+// function name(){
+//     let name="amrit";
+//     let count=0;
+//     function printname(){
+//         if(count===5) return        
+//         console.log(name);
+//         count++;
+//         printname()
+//     }
+//     printname()
+// }
+// name()
