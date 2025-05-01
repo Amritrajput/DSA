d631832c1f115bbb835dcbebdf2fb25e1a5bfc57
amrit raj <amritrajsingh179@gmail.com>
2025-05-01 21:41:37 +0530

    parameter-wise

--- recursionPatterns/sumofirstnnumber.js
+++ recursionPatterns/sumofirstnnumber.js
@@ -1,3 +1,5 @@
+// sumofirstnnumber(using function)
+
 // function sum(n) {
 // function sumofn(k){
 //     if(k===0) return 0;
@@ -6,3 +8,14 @@
 // console.log(sumofn(n));
 // }
 // sum(5)
+
+// sumofirstnnumber(parameter-wise)
+
+// function sumofn(i,sum){
+//     if(i<1){
+//         console.log(sum);
+//         return;
+//     }
+//    sumofn(i-1,sum+i);
+// }
+// sumofn(5,0)
