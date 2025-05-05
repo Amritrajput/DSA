d2a5f35b6a488e27bb71de91ab935cb80ca02685
amrit raj <amritrajsingh179@gmail.com>
2025-05-05 23:02:38 +0530

    modified question

--- recursionPatterns/printingsubsequencewhosesumisk.js
+++ recursionPatterns/printingsubsequencewhosesumisk.js
@@ -1,19 +1,53 @@
-let arr = [3,2,4,5,6,7]
-let n = arr.length;
-let sum=12
-function subsequencesum(ind,array,s){
-    if(ind == n){
-      if(s===sum){
-        console.log(array);
-      }
-        return;
-      }
-      array.push(arr[ind]);
-      s+=arr[ind];
-      subsequencesum(ind+1,array,s);
-      array.pop();
-      s-=arr[ind];
-      subsequencesum(ind+1,array,s);
-     }
+// printing all subsequence whose sum is k
 
-subsequencesum(0,[],0)
+
+// let arr = [3,2,4,5,6,7]
+// let n = arr.length;
+// let sum=12
+// function subsequencesum(ind,array,s){
+//     if(ind == n){
+//       if(s===sum){
+//         console.log(array);
+//       }
+//         return;
+//       }
+//       array.push(arr[ind]);
+//       s+=arr[ind];
+//       subsequencesum(ind+1,array,s);
+//       array.pop();
+//       s-=arr[ind];
+//       subsequencesum(ind+1,array,s);
+//      }
+
+// subsequencesum(0,[],0)
+
+
+// printing only one subsequence whose is sum is k
+
+
+
+let arr = [3, 2, 4, 5, 6, 7];
+let n = arr.length;
+let sum = 12;
+
+function subsequenceSum(ind, array, s) {
+    if (ind === n) {
+        if (s === sum) {
+            console.log(array);
+            return true;
+        }
+        return false;
+    }
+
+    array.push(arr[ind]);
+    s += arr[ind];
+    if (subsequenceSum(ind + 1, array, s)) return true;
+
+    array.pop();
+    s -= arr[ind];
+    if (subsequenceSum(ind + 1, array, s)) return true;
+
+    return false;
+}
+
+subsequenceSum(0, [], 0);
